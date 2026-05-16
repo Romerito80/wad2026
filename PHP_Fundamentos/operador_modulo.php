e67eced0837c7ddc83c7309dfa97658f93fbdfb7
rauldiazromeroal-star <raul.diaz.romero.al@iespoligonosur.org>
2026-05-16 19:07:25 +0200

    commit 16/05/26

---
+++ PHP_Fundamentos/operador_modulo.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Prácticas PHP</title>
+</head>
+
+<body>
+
+<?php
+
+    $num1 = 3;
+    $num2 = 10;
+
+    //Suma con operador aritmético
+    $total = $num1 + $num2;
+    echo $total;
+
+    $resultado = $num1 % $num2;
+    echo $resultado;
+
+?>
+
+</body>
+</html>
